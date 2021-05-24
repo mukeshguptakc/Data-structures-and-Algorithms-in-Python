3095f49476342c18ca91831aed968122bbb1a16a
mukeshguptakc <guptamukeshkc@gmail.com>
2021-05-24 10:15:47 +0530

    Implement Tower of Hanoi using recursion.

---
+++ 02_Recursion/tower_of_hanoi.py
@@ -0,0 +1,11 @@
+
+def TowerOfHanoi(disk, _from, _to, aux):
+    if(disk == 1):
+        print("Move disk", disk ,"from", _from, "to", _to)
+        return
+    TowerOfHanoi(disk-1, _from, aux, _to)
+    print("Move disk", disk,"from", _from, "to", _to)
+    TowerOfHanoi(disk-1, aux, _to, _from)
+
+disk = 4
+TowerOfHanoi(disk, "A", "B", "C")
